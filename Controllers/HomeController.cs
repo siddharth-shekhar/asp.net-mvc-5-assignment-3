db55a062ff32f66faf212d90c1ff70016e4657c0
Siddharth Shekhar <33785950+siddharth-shekhar@users.noreply.github.com>
2022-05-06 12:41:20 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,37 +10,36 @@ namespace Assignment1_ASP.NET_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
-        {
-            ViewData["str1"] = "This is a String passed using viewData";
-            ViewData["num1"] = 100;
+                    {
+                        ViewData["str1"] = "This is a String passed using viewData";
+                        ViewData["num1"] = 100;
 
-            ViewBag.str2 = "This is a string passed using ViewBag";
-            ViewBag.num2 = 200;
-            return View();
-        }
+                        ViewBag.str2 = "This is a string passed using ViewBag";
+                        ViewBag.num2 = 200;
+                        return View();
+                    }
 
         public ActionResult AddUser()
-        {
-            return View();
-        }
+                    {
+                        return View();
+                    }
 
         public ActionResult SaveUser(User u)
-        {
-            StreamWriter sw = new StreamWriter(Server.MapPath("~/App_Data/users.txt"), true);
-            sw.WriteLine("User details added on : " + DateTime.Now.ToString());
-            sw.WriteLine("User Name : " + u.UserName);
-            sw.WriteLine("Password : " + u.Password);
-            sw.WriteLine();
-            sw.Close();
-            return Content("User details have been Saved!!");
-        }
+                 {
+                        StreamWriter sw = new StreamWriter(Server.MapPath("~/App_Data/users.txt"), true);
+                        sw.WriteLine("User details added on : " + DateTime.Now.ToString());
+                        sw.WriteLine("User Name : " + u.UserName);
+                        sw.WriteLine("Password : " + u.Password);
+                        sw.WriteLine();
+                        sw.Close();
+                        return Content("User details have been Saved!!");
+               }
 
-        //just for demonstration HtmlHelpers
-        public ActionResult HtmlHelpers()
-        {
-            return View();
-        }
+        public ActionResult HtmlHelpers()             //just for demonstration HtmlHelpers
+
+                 {
+                        return View();
+                }
     }
 }
